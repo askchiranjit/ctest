6ecbd283206d3d7d39b1830b667f800bcc73f844
Pratap <Pratap@Pratap-PC>
2015-04-10 13:40:11 +0530

    Added the listner to check test run mode

---
+++ src/test/java/com/ensimtest/listners/TestCaseRunmode.java
@@ -0,0 +1,29 @@
+package com.ensimtest.listners;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+
+import com.ensimtest.utils.TestUtils;
+import com.ensimtest.utils.Xls_Reader;
+
+public class TestCaseRunmode implements IAnnotationTransformer {
+
+	@Override
+	public void transform(ITestAnnotation anotation_name, Class class_name, Constructor constructor_name,Method methode_name)
+	{
+		TestUtils tu=new TestUtils();
+		String C_name=methode_name.getDeclaringClass().getSimpleName();
+		Xls_Reader xr=new Xls_Reader("resources\\testdata\\"+C_name+".xlsx");
+		String M_name=methode_name.getName();
+		if(tu.isTestCaseRunnable(xr,M_name)==false)
+		{
+			anotation_name.setEnabled(false);
+		}
+		xr=null;
+		tu=null;
+	}
+
+}
