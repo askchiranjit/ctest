628db3adc94378769e79d30530d578c1284f8d58
Pratap <Pratap@Pratap-PC>
2015-04-18 01:59:18 +0530

    Added xpaths for all the links under catalog menu

--- src/test/java/com/ensimtest/module/entities/CatalogOptions.java
+++ src/test/java/com/ensimtest/module/entities/CatalogOptions.java
@@ -1,5 +1,6 @@
 package com.ensimtest.module.entities;
 
+import com.ensimtest.config.Element;
 import com.ensimtest.resource.AppData;
 /**
  * This will open the Catalog menus
@@ -9,5 +10,52 @@ import com.ensimtest.resource.AppData;
 
 public class CatalogOptions extends AppData
 {
+	public class CatalogMenu extends Element
+	{
+		public CatalogMenu()
+		{
+			setElement(readAppData(this));
+		}
+		public void mouseHover()
+		{
+			super.mouseHover();
+		}
+	}
+	public CatalogMenu catalogMenu=new CatalogMenu();
 
+	public class OffersLnk extends Element
+	{
+		public OffersLnk()
+		{
+			setElement(readAppData(this));
+		}
+	}
+	public OffersLnk offersLnk = new OffersLnk();
+
+	public class RegionsLnk extends Element
+	{
+		public RegionsLnk()
+		{
+			setElement(readAppData(this));
+		}
+	}
+	public RegionsLnk regionsLnk = new RegionsLnk();
+
+	public class ItemsAndRPPLnk extends Element
+	{
+		public ItemsAndRPPLnk()
+		{
+			setElement(readAppData(this));
+		}
+	}
+	public ItemsAndRPPLnk itemsAndRPPLnk = new ItemsAndRPPLnk();
+
+	public class Serviceslnk extends Element
+	{
+		public Serviceslnk()
+		{
+			setElement(readAppData(this));
+		}
+	}
+	public Serviceslnk serviceslnk = new Serviceslnk();
 }
