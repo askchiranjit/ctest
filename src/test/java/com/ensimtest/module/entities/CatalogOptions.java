eb53b2a29eb8c580e797beeb84c9cd73130e73b6
Pratap <Pratap@Pratap-PC>
2015-04-18 01:20:58 +0530

    Added xml file for Catalog menus

---
+++ src/test/java/com/ensimtest/module/entities/CatalogOptions.java
@@ -0,0 +1,13 @@
+package com.ensimtest.module.entities;
+
+import com.ensimtest.resource.AppData;
+/**
+ * This will open the Catalog menus
+ * @author Pratap
+ *
+ */
+
+public class CatalogOptions extends AppData
+{
+
+}
