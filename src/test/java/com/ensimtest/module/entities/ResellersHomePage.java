199aab2d9a621ec45003176a5c22cbfb6370464c
Pratap <Pratap@Pratap-PC>
2015-04-18 17:14:02 +0530

    Added class and controls for Reseller home page

---
+++ src/test/java/com/ensimtest/module/entities/ResellersHomePage.java
@@ -0,0 +1,22 @@
+package com.ensimtest.module.entities;
+
+import com.ensimtest.config.Element;
+import com.ensimtest.module.entities.AgentHomePage.AddAgentBtn;
+import com.ensimtest.resource.AppData;
+
+/**
+ * This is for Resellers Home page
+ * @author Pratap
+ *
+ */
+public class ResellersHomePage extends AppData
+{
+	public class AddResellerBtn extends Element
+	{
+		public AddResellerBtn()
+		{
+			setElement(readAppData(this));
+		}
+	}
+   public AddResellerBtn addResellerBtn=new AddResellerBtn();
+}
