8c8efb700ac5bfeb031fe69d15eb159d09a1097b
Pratap <Pratap@Pratap-PC>
2015-04-20 15:20:13 +0530

    updated

--- src/test/java/com/ensimtest/module/authentication/SignUpScreen.java
+++ src/test/java/com/ensimtest/module/authentication/SignUpScreen.java
@@ -66,7 +66,7 @@ public class SignUpScreen extends AppData
 		
 		public void selectCountryName(String countryName)
 		{
-			selectValue(countryName);
+			selectVisibleText(countryName);
 		}
 	}
 	public Country countryList = new Country();
