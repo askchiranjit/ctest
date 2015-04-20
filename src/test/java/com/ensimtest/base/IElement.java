0d6663816f8b156a1b9b9789ba0d61dc76edf5e8
Pratap <Pratap@Pratap-PC>
2015-04-20 14:33:43 +0530

    isEnabled method introduced

--- src/test/java/com/ensimtest/base/IElement.java
+++ src/test/java/com/ensimtest/base/IElement.java
@@ -5,6 +5,7 @@ public interface IElement
 	public void click();
 	public boolean isExists();
 	public boolean isDisplayed();
+	public boolean isEnabled();
 	public String read();
 	public void write(String s);
 }
