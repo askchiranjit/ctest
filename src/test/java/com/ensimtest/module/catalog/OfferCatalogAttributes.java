7ea92d2d215421bddc94877862177073f9971e28
Dip <Dip@Dipayan>
2015-07-20 19:08:12 +0530

    added and updated

--- src/test/java/com/ensimtest/module/catalog/OfferCatalogAttributes.java
+++ src/test/java/com/ensimtest/module/catalog/OfferCatalogAttributes.java
@@ -1,5 +1,73 @@
 package com.ensimtest.module.catalog;
 
-public class OfferCatalogAttributes {
+import com.ensimtest.config.Element;
+import com.ensimtest.resource.AppData;
+
+/**
+ * To handle all controls under catalog attributes properties
+ * @author Dip
+ *
+ */
+public class OfferCatalogAttributes extends AppData{
+	
+	/*
+	 * Display Order text box
+	 */
+	public class DisplayOrderTxt extends Element{
+		public DisplayOrderTxt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public DisplayOrderTxt displayOrderTxt = new DisplayOrderTxt();
+	
+	/*
+	 * Display Text
+	 */
+	public class DisplayTextTxt extends Element{
+		public DisplayTextTxt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public DisplayTextTxt displayTextTxt = new DisplayTextTxt();
+	
+	/*
+	 * Add icon
+	 */
+	public class AddDisplayTextBtn extends Element{
+		public AddDisplayTextBtn(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public AddDisplayTextBtn addDisplayTextBtn = new AddDisplayTextBtn();
+	
+	/*
+	 * Remove icon
+	 */
+	public class RemoveDisplayTextBtn extends Element{
+		public RemoveDisplayTextBtn(){}
+	}
+	
+	public RemoveDisplayTextBtn removeDisplayTextBtn = new RemoveDisplayTextBtn();
+	
+	/*
+	 * To write values in the text box and add that one
+	 */
+	public void writeOfferFeaturesToDisplay(String[] displayOrderLst, String[] displayTextLst){
+		
+		for(int i=0;i<displayOrderLst.length;i++){
+			
+			displayOrderTxt.write(displayOrderLst[i]);
+			displayTextTxt.write(displayTextLst[i]);
+			addDisplayTextBtn.click();
+		}
+	}
+	
+	/*
+	 * To edit featured values
+	 */
+//TODO:tom
 
 }
--- src/test/java/com/ensimtest/module/catalog/OfferTermsAndNotificationMacro.java
+++ src/test/java/com/ensimtest/module/catalog/OfferTermsAndNotificationMacro.java
@@ -1,5 +1,263 @@
 package com.ensimtest.module.catalog;
 
-public class OfferTermsAndNotificationMacro {
+import com.ensimtest.config.Element;
+import com.ensimtest.resource.AppData;
 
+/**
+ * To handle all controls of Terms and Notification Macros
+ * @author Dip
+ *
+ */
+public class OfferTermsAndNotificationMacro extends AppData{
+	
+	/*
+	 * Provisioning actions
+	 */
+	public class ProvisioningActionsChk extends Element{
+		public ProvisioningActionsChk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public ProvisioningActionsChk provisioningActionsChk = new ProvisioningActionsChk();
+	
+	public class ProvisioningActionsTxt extends Element{
+		public ProvisioningActionsTxt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public ProvisioningActionsTxt provisioningActionsTxt = new ProvisioningActionsTxt();
+	
+//-------------------------------------------------------------------------------------------------------
+	
+	/*
+	 * Opt-out trial disclaimer
+	 */
+	public class OptOutTrialDisclaimerChk extends Element{
+		public OptOutTrialDisclaimerChk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public OptOutTrialDisclaimerChk optOutTrialDisclaimerChk = new OptOutTrialDisclaimerChk();
+	
+	public class OptOutTrialDisclaimerTxt extends Element{
+		public OptOutTrialDisclaimerTxt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public OptOutTrialDisclaimerTxt optOutTrialDisclaimerTxt = new OptOutTrialDisclaimerTxt();
+
+//------------------------------------------------------------------------------------------------------
+	
+	/*
+	 * Opt-in trial disclaimer
+	 */
+	public class OptInTrialDisclaimerChk extends Element{
+		public OptInTrialDisclaimerChk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public OptInTrialDisclaimerChk optInTrialDisclaimerChk = new OptInTrialDisclaimerChk();
+	
+	public class OptInTrialDisclaimerTxt extends Element{
+		public OptInTrialDisclaimerTxt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public OptInTrialDisclaimerTxt optInTrialDisclaimerTxt = new OptInTrialDisclaimerTxt();
+	
+//---------------------------------------------------------------------------------------------------------
+	
+	/*
+	 * Offer description
+	 */
+	public class OfferDescriptionChk extends Element{
+		public OfferDescriptionChk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public OfferDescriptionChk offerDescriptionChk = new OfferDescriptionChk();
+	
+	public class OfferDescriptionTxt extends Element{
+		public OfferDescriptionTxt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public OfferDescriptionTxt offerDescriptionTxt = new OfferDescriptionTxt();
+
+//-------------------------------------------------------------------------------------------------------------
+	
+	/*
+	 * How to use
+	 */
+	public class HowToUseChk extends Element{
+		public HowToUseChk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public HowToUseChk howToUseChk = new HowToUseChk();
+	
+	public class HowToUseTxt extends Element{
+		public HowToUseTxt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public HowToUseTxt howToUseTxt = new HowToUseTxt();	
+	
+//---------------------------------------------------------------------------------------------------------------
+	
+	/*
+	 * Terms & Conditions 
+	 */
+	public class TermsAndConditionsChk extends Element{
+		public TermsAndConditionsChk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public TermsAndConditionsChk termsAndConditionsChk = new TermsAndConditionsChk();
+	
+	public class TermsAndConditionsTxt extends Element{
+		public TermsAndConditionsTxt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public TermsAndConditionsTxt termsAndConditionsTxt = new TermsAndConditionsTxt();	
+	
+//---------------------------------------------------------------------------------------------------------------
+	
+	/*
+	 * Custom-Macro-1 
+	 */
+	public class CustomMacro1Chk extends Element{
+		public CustomMacro1Chk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CustomMacro1Chk customMacro1Chk = new CustomMacro1Chk();
+	
+	public class CustomMacro1Txt extends Element{
+		public CustomMacro1Txt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CustomMacro1Txt customMacro1Txt = new CustomMacro1Txt();	
+	
+//----------------------------------------------------------------------------------------------------------------
+
+	/*
+	 * Custom-Macro-2 
+	 */
+	public class CustomMacro2Chk extends Element{
+		public CustomMacro2Chk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CustomMacro2Chk customMacro2Chk = new CustomMacro2Chk();
+	
+	public class CustomMacro2Txt extends Element{
+		public CustomMacro2Txt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CustomMacro2Txt customMacro2Txt = new CustomMacro2Txt();	
+	
+//----------------------------------------------------------------------------------------------------------------
+	
+	/*
+	 * Custom-Macro-3 
+	 */
+	public class CustomMacro3Chk extends Element{
+		public CustomMacro3Chk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CustomMacro3Chk customMacro3Chk = new CustomMacro3Chk();
+	
+	public class CustomMacro3Txt extends Element{
+		public CustomMacro3Txt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CustomMacro3Txt customMacro3Txt = new CustomMacro3Txt();	
+	
+//----------------------------------------------------------------------------------------------------------------
+	
+	/*
+	 * Custom-Macro-4 
+	 */
+	public class CustomMacro4Chk extends Element{
+		public CustomMacro4Chk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CustomMacro4Chk customMacro4Chk = new CustomMacro4Chk();
+	
+	public class CustomMacro4Txt extends Element{
+		public CustomMacro4Txt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CustomMacro4Txt customMacro4Txt = new CustomMacro4Txt();	
+	
+//----------------------------------------------------------------------------------------------------------------
+	
+	/*
+	 * Custom-Macro-5 
+	 */
+	public class CustomMacro5Chk extends Element{
+		public CustomMacro5Chk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CustomMacro5Chk customMacro5Chk = new CustomMacro5Chk();
+	
+	public class CustomMacro5Txt extends Element{
+		public CustomMacro5Txt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CustomMacro5Txt customMacro5Txt = new CustomMacro5Txt();	
+	
+//----------------------------------------------------------------------------------------------------------------
+	
+	/*
+	 * Custom-Macro-6 
+	 */
+	public class CustomMacro6Chk extends Element{
+		public CustomMacro6Chk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CustomMacro6Chk customMacro6Chk = new CustomMacro6Chk();
+	
+	public class CustomMacro6Txt extends Element{
+		public CustomMacro6Txt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CustomMacro6Txt customMacro6Txt = new CustomMacro6Txt();	
+	
 }
