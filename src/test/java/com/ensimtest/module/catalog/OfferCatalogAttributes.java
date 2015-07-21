983e0fae94032c976f11e4d2dca159eee11485ce
Dip <Dip@Dipayan>
2015-07-21 16:53:24 +0530

    added and updated

--- src/test/java/com/ensimtest/module/catalog/OfferCatalogAttributes.java
+++ src/test/java/com/ensimtest/module/catalog/OfferCatalogAttributes.java
@@ -14,8 +14,11 @@ public class OfferCatalogAttributes extends AppData{
 	 * Display Order text box
 	 */
 	public class DisplayOrderTxt extends Element{
-		public DisplayOrderTxt(){
-			setElement(readAppData(this));
+		public DisplayOrderTxt(){}
+		
+		public void setID(int count){
+			String id = "features." + count + ".displayorder";
+			setElement("ID", id);
 		}
 	}
 	
@@ -25,8 +28,11 @@ public class OfferCatalogAttributes extends AppData{
 	 * Display Text
 	 */
 	public class DisplayTextTxt extends Element{
-		public DisplayTextTxt(){
-			setElement(readAppData(this));
+		public DisplayTextTxt(){}
+		
+		public void setID(int count){
+			String id = "features." + count + ".featuredisplayText";
+			setElement("ID", id);
 		}
 	}
 	
@@ -57,6 +63,9 @@ public class OfferCatalogAttributes extends AppData{
 	 */
 	public void writeOfferFeaturesToDisplay(String[] displayOrderLst, String[] displayTextLst){
 		
+		displayOrderTxt.setID(0);
+		displayTextTxt.setID(0);
+		
 		for(int i=0;i<displayOrderLst.length;i++){
 			
 			displayOrderTxt.write(displayOrderLst[i]);
@@ -68,6 +77,19 @@ public class OfferCatalogAttributes extends AppData{
 	/*
 	 * To edit featured values
 	 */
-//TODO:tom
+	public void editOfferFeaturesToDisplay(String[] oldDisplayTextLst, String[] newDisplayTextLst){
+	
+		for(int i=0;i<oldDisplayTextLst.length;i++){
+			
+			displayTextTxt.setID(i+1);
+			
+			String oldValue = displayTextTxt.getAttributeValue("value");
+			
+			if(oldDisplayTextLst[i].equalsIgnoreCase(oldValue)){
+				displayTextTxt.clearAllChars(oldValue.length());
+				displayTextTxt.write(newDisplayTextLst[i]);
+			}
+		}
+	}
 
 }
--- src/test/java/com/ensimtest/module/catalog/OfferMarketplaceContentProperties.java
+++ src/test/java/com/ensimtest/module/catalog/OfferMarketplaceContentProperties.java
@@ -1,5 +1,212 @@
 package com.ensimtest.module.catalog;
 
-public class OfferMarketplaceContentProperties {
+import com.ensimtest.config.Element;
+import com.ensimtest.resource.AppData;
 
+/**
+ * To handle all the elements under market place properties
+ * @author Dip
+ *
+ */
+public class OfferMarketplaceContentProperties extends AppData{
+
+	/*
+	 * Commercial info link
+	 */
+	public class CommercialInfoLinkTxt extends Element{
+		public CommercialInfoLinkTxt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CommercialInfoLinkTxt commercialInfoLinkTxt = new CommercialInfoLinkTxt();
+	
+	/*
+	 * Display in Catalog
+	 */
+	public class DisplayInCatalogChk extends Element{
+		public DisplayInCatalogChk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public DisplayInCatalogChk displayInCatalogChk = new DisplayInCatalogChk();
+	
+	/*
+	 * Display as Featured Service
+	 */
+	public class DisplayAsFeaturedServiceChk extends Element{
+		public DisplayAsFeaturedServiceChk(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public DisplayAsFeaturedServiceChk displayAsFeaturedServiceChk = new DisplayAsFeaturedServiceChk();
+	
+	/*
+	 * Rank
+	 */
+	public class RankTxt extends Element{
+		public RankTxt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public RankTxt rankTxt = new RankTxt();
+	
+	/*
+	 * Image URL
+	 */
+	public class ImageURLBtn extends Element{
+		public ImageURLBtn(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public ImageURLBtn imageURLBtn = new ImageURLBtn();
+	
+//----------------------------------------------Add Image URL pop-up-----------------------------------------------
+	/*
+	 * Cross the pop up
+	 */
+	public class CrossButtonOfPopUpBtn extends Element{
+		public CrossButtonOfPopUpBtn(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CrossButtonOfPopUpBtn crossButtonOfPopUpBtn = new CrossButtonOfPopUpBtn();
+	
+	/*
+	 * Small image URL text box
+	 */
+	public class SmallImageURLTxt extends Element{
+		public SmallImageURLTxt(){
+			setElement(readAppData(this));
+		}
+		
+		/**
+		 * To write and edit URL
+		 * @param data
+		 */
+		public void writeURL(String data){
+			String str = super.getAttributeValue("value");
+			super.clearAllChars(str.length());
+			super.write(data);
+		}
+	}
+	
+	public SmallImageURLTxt smallImageURLTxt = new SmallImageURLTxt();
+	
+	/*
+	 * Medium image URL text box
+	 */
+	public class MediumImageURLTxt extends Element{
+		public MediumImageURLTxt(){
+			setElement(readAppData(this));
+		}
+		
+		/**
+		 * To write and edit URL
+		 * @param data
+		 */
+		public void writeURL(String data){
+			String str = super.getAttributeValue("value");
+			super.clearAllChars(str.length());
+			super.write(data);
+		}
+	}
+	
+	public MediumImageURLTxt mediumImageURLTxt = new MediumImageURLTxt();
+	
+	/*
+	 * Rectangle image URL text box
+	 */
+	public class RectangleImageURLTxt extends Element{
+		public RectangleImageURLTxt(){
+			setElement(readAppData(this));
+		}
+		
+		/**
+		 * To write and edit URL
+		 * @param data
+		 */
+		public void writeURL(String data){
+			String str = super.getAttributeValue("value");
+			super.clearAllChars(str.length());
+			super.write(data);
+		}
+	}
+	
+	public RectangleImageURLTxt rectangleImageURLTxt = new RectangleImageURLTxt();
+	
+	/*
+	 * To save image URL OK button
+	 */
+	public class OKButtonToSaveImageURLBtn extends Element{
+		public OKButtonToSaveImageURLBtn(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public OKButtonToSaveImageURLBtn okButtonToSaveImageURLBtn = new OKButtonToSaveImageURLBtn();
+	
+	/*
+	 * To cancel the changes in image URL Cancel button
+	 */
+	public class CancelButtonToSaveImageURLBtn extends Element{
+		public CancelButtonToSaveImageURLBtn(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CancelButtonToSaveImageURLBtn cancelButtonToSaveImageURLBtn = new CancelButtonToSaveImageURLBtn();
+
+//-----------------------------------------------END of pop up--------------------------------------------------------
+	
+	/*
+	 * Price tag in catalog
+	 */
+	public class CurrencyTagSelectLnk extends Element{
+		public CurrencyTagSelectLnk(){}
+		
+		/**
+		 * Select currency tag
+		 * @param currency
+		 */
+		public void selectCurrencyTag(String currency){
+			String xpath = "//div[@id='pricetag']//table//tbody//tr//td[@class='mappedCurrency']//span[@id='" + currency + "']";
+			setElement("Xpath", xpath);
+			super.click();
+		}
+	}
+	
+	public CurrencyTagSelectLnk currencyTagSelectLnk = new CurrencyTagSelectLnk();
+	
+	/*
+	 * To provide info in text area
+	 */
+	public class CurrencyTextAreaTxt extends Element{
+		public CurrencyTextAreaTxt(){
+			setElement(readAppData(this));
+		}
+	}
+	
+	public CurrencyTextAreaTxt currencyTextAreaTxt = new CurrencyTextAreaTxt();
+	
+	/**
+	 * To write information in market place currency tag area
+	 * @param currencyLst
+	 * @param textValueLst
+	 */
+	public void writeCurrencyTagForMarketPlace(String[] currencyLst, String[] textValueLst){
+		
+		for(int i=0;i<currencyLst.length;i++){
+			
+			currencyTagSelectLnk.selectCurrencyTag(currencyLst[i]);
+			
+			currencyTextAreaTxt.clear();
+			currencyTextAreaTxt.write(textValueLst[i]);
+		}
+	}
 }
